95cc6971c256b8a1f4736f430f64558e409fc890
Diego Asensio <diegonjin@gmail.com>
2017-09-14 00:56:49 +0200

    Updating web

---
+++ app/scripts/main.js
@@ -0,0 +1,54 @@
+$(function() {
+
+    loadPlayer();
+
+      function loadPlayer() { 
+        if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
+      
+          var tag = document.createElement('script');
+          tag.src = "https://www.youtube.com/iframe_api";
+          var firstScriptTag = document.getElementsByTagName('script')[0];
+          firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      
+          window.onYouTubePlayerAPIReady = function() {
+            onYouTubePlayer();
+          };
+      
+        } else {
+      
+          onYouTubePlayer();
+      
+        }
+      }
+    
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  var player1 = null;
+  
+    function onYouTubePlayer() {
+        player1 = new YT.Player('player');
+        console.log('4nto')
+    }
+
+
+  $('.launch-modal').on('click', function(e){
+    e.preventDefault();
+    $( '#' + $(this).data('modal-id') ).modal();
+    });
+
+    $('#modal-video').on('hidden.bs.modal', function () {
+        // do something
+        player1.stopVideo();
+      })
+
+});
