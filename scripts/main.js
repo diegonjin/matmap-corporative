5a97dd3100b2a548e1c68c3f51ef85dd0e2d0a48
Diego Asensio <diegonjin@gmail.com>
2017-09-21 08:19:41 +0200

    Updating changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 $(function() {
 
     loadPlayer();
+    initializeEvents();
+    initializeDB();
 
-      function loadPlayer() { 
+    function loadPlayer() { 
         if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
       
           var tag = document.createElement('script');
@@ -40,15 +42,47 @@ $(function() {
         console.log('4nto')
     }
 
-
-  $('.launch-modal').on('click', function(e){
-    e.preventDefault();
-    $( '#' + $(this).data('modal-id') ).modal();
+  function initializeEvents() {
+    $('#btn-add-newsletters').on('click', function() {
+      saveEmail();
+    });
+  
+    $('.launch-modal').on('click', function(e){
+      e.preventDefault();
+      $( '#' + $(this).data('modal-id') ).modal();
+    });
+    
+    $('#modal-video').on('hidden.bs.modal', function () {
+      // do something
+      player1.stopVideo();
+    }); 
+    
+    $('#terms-and-conditions-link').on('click', function(e){
+      e.preventDefault();
+      $( '#' + $(this).data('modal-id') ).modal();
     });
 
-    $('#modal-video').on('hidden.bs.modal', function () {
-        // do something
-        player1.stopVideo();
-      })
+    $('#legal-advice-link').on('click', function(e){
+      e.preventDefault();
+      $( '#' + $(this).data('modal-id') ).modal();
+    });
+  }
 
+  function initializeDB() {
+    var config = {
+      apiKey: "AIzaSyD31JyppZt5CSEMAbiTm57LStvJhmREquE",
+      authDomain: "matmapco-f672a.firebaseapp.com",
+      databaseURL: "https://matmapco-f672a.firebaseio.com",
+      projectId: "matmapco-f672a",
+      storageBucket: "matmapco-f672a.appspot.com",
+      messagingSenderId: "1052988661423"
+    };
+    firebase.initializeApp(config);
+  }
+
+  function saveEmail() {
+    var inputEmail = $('#input-email').val();
+    var database = firebase.database().ref();
+    database.child('emails').push(inputEmail);
+  }
 });
